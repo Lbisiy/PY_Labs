ff7d03f9711f49175186bb0b24832f3d6f3ed359
Marat <115390373+Lbisiy@users.noreply.github.com>
2022-11-11 17:46:58 +0500

    Крестики-нолики измененный logic.new.py
    Уменьшил код в функции def draw_field()

--- logic_new.py
+++ logic_new.py
@@ -1,4 +1,11 @@
 def check_move(current_player: str, INDEX: int, field: list) -> int:
+    """
+    Функция проверок ходов игроков (правильная/неправильная, занята/не занята)
+    :param current_player: 'крестик' или 'нолик'
+    :param INDEX: Размерность поля
+    :param field: Инициализированное поле с ходами игроков в виде list
+    :return: Ход игрока для размещения на поле
+    """
     while True:
         move = int(input(f'Чтобы поставить {current_player} укажите номер ячейки:'))
         if move > pow(INDEX, 2) or move < 0:
@@ -18,19 +25,12 @@ def draw_field(field: list) -> None:
     :param field: Подается на вход изначально заданный пустой list, а затем иземенный list после ходов игроков
     :return None: Печатает начальное и поле после каждого хода
     """
-    print(field[0], '|', end=' ')
-    print(field[1], '|', end=' ')
-    print(field[2])
-    print('---------')
-
-    print(field[3], '|', end=' ')
-    print(field[4], '|', end=' ')
-    print(field[5])
-    print('---------')
-
-    print(field[6], '|', end=' ')
-    print(field[7], '|', end=' ')
-    print(field[8])
+    for i, value in enumerate(field):
+        print(' |', value, end='')
+        if i == 2 or i == 5 or i == 8:
+            print(' |')
+            print('---------------')
+            continue
 
 def who_win(field: list) -> int:
     """
@@ -61,14 +61,14 @@ def game_continue(win: int, move: int) -> int:
     """
     Функция проверки окончания игры после каждого хода
     :param win: Результат функции проверки победы. 1-первый игрок выиграл, 2-второй, 0-никто
-    :param move: Введенный игроками номер клетки поля от 0 до 8
+    :param move: Введенный игроками номер клетки поля от 1 до 9
     :return: 0 - игра продолжается, 1 - игра окончена
     """
-    if win == 0 and move != 10:
+    if win == 0:
         continue_ = 0
         print(f'Игра продолжается: {continue_}')
         return continue_
-    elif win != 0 or move == 10:
+    elif win != 0:
         continue_ = 1
         print(f'Игра окончена: {continue_}')
         return continue_
