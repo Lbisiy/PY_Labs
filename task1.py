7e6c33a92fce945d9f27810fa04733483ce84bde
Marat <115390373+Lbisiy@users.noreply.github.com>
2022-11-11 12:27:52 +0500

    Лабораторная работа_тема_5_побежденный task1
    Добавил побежденный task1

--- task1.py
+++ task1.py
@@ -3,17 +3,9 @@ from pprint import pprint
 
 def list_of_dict(n: int=15) -> None:
     n = 15
-    num = [i for i in range(0, n+1)]
-    dict_ = {'bin': 0, 'dec': 0, 'hex': 0, 'oct': 0}
-    list_ = []
-
-    for n in range(len(num)):
-        dict_['bin'] = bin(n)
-        dict_['dec'] = n
-        dict_['hex'] = hex(n)
-        dict_['oct'] = oct(n)
-        list_.append(str(dict_))
-
+    list_ = [{'bin': bin(i), 'dec': i, 'hex': hex(i), 'oct': oct(i)} for i in range(n+1)]
     return pprint(list_)
 
 list_of_dict()
+
+
