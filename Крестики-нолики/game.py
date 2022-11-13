62961ec20bd0fbf62f872bdf2631b7fb217c42bc
Marat <115390373+Lbisiy@users.noreply.github.com>
2022-11-14 00:16:55 +0500

    Крестики-нолики после всех доработок
    Добавлен файл game.py. Вроде как реализован весь необходимый функционал

--- Крестики-нолики/game.py
+++ Крестики-нолики/game.py
@@ -4,14 +4,14 @@ from typing import List
 
 def check_move(current_player: str, index: int, field: List[str]) -> int:
     """
-    Функция проверок ходов игроков (правильная/неправильная, занята/не занята)
+    Функция проверок ходов игроков
     :param current_player: 'крестик' или 'нолик'
-    :param INDEX: Размерность поля
-    :param field: Инициализированное поле с ходами игроков в виде list
+    :param index: Размерность поля от 3 до 5
+    :param field: Инициализированное поле с ходами игроков
     :return: Ход игрока для размещения на поле
     """
     while True:
-        move = int(input(f'Чтобы поставить {current_player} укажите номер ячейки:'))
+        move = int(input(f'Чтобы поставить {current_player} укажите номер ячейки от 1 до {pow(index, 2)}:'))
         if move > pow(index, 2) or move < 0:
             print('Вы ввели неправильный номер ячейки')
             continue
@@ -22,15 +22,15 @@ def check_move(current_player: str, index: int, field: List[str]) -> int:
     return move
 
 
-def draw_field(field: list, index: int) -> None:
+def draw_field(field: List[str], index: int) -> None:
     """
     Функция отрисовки поля
     Поле фиксированного размера 3х3
 
-    :param field: Подается на вход изначально заданный пустой list, а затем иземенный list после ходов игроков
-    :return None: Печатает начальное и поле после каждого хода
+    :param field: При начале игры - пустое поле, затем с ходами игроков
+    :param index: Размерность поля от 3 до 5
+    :return None: Печатает поле
     """
-    i = 0
     field = [field[i:i + index] for i in range(0, index ** 2, index)]
     for row in field:
         for cell in row:
@@ -38,38 +38,40 @@ def draw_field(field: list, index: int) -> None:
         print('|')
 
 
-def who_win(field: List[str]) -> int:
+def who_win(field: List[str], index: int) -> int:
     """
     Функция проверки победы
     :param field: Поле после хода игрока
-    :return win: Возвращает 1-первый игрок выиграл, 2-второй, 0-никто
+    :param index: Размерность поля
+    :return win: 1(первый игрок выиграл), 2(второй) или 0(никто)
     """
-    if field[0] == field[1] == field[2] == 'X' or field[3] == field[4] == field[5] == 'X' or \
-            field[6] == field[7] == field[8] == 'X' or field[0] == field[3] == field[6] == 'X' or \
-            field[1] == field[4] == field[7] == 'X' or field[2] == field[5] == field[8] == 'X' or \
-            field[0] == field[4] == field[8] == 'X' or field[2] == field[4] == field[6] == 'X':
-        win = 1
-        print('Выиграл игрок: 1')
-        return win
-    elif field[0] == field[1] == field[2] == '0' or field[3] == field[4] == field[5] == '0' or \
-            field[6] == field[7] == field[8] == '0' or field[0] == field[3] == field[6] == '0' or \
-            field[1] == field[4] == field[7] == '0' or field[2] == field[5] == field[8] == '0' or \
-            field[0] == field[4] == field[8] == '0' or field[2] == field[4] == field[6] == '0':
-        win = 2
-        print('Выиграл игрок: 2')
-        return win
-    else:
-        win = 0
-        print('Выиграл игрок: 0')
-        return win
+    field_ = [field[i:i + index] for i in range(0, index ** 2, index)]
+    field_trans = [[field_[j][i] for j in range(len(field_))] for i in range(len(field_[0]))]
+    field_diagonal_first = [field[i] for i in range(0, index ** 2, index + 1)]
+    field_diagonal_second = [field[i] for i in range(index - 1, ((index ** 2) - index + 1), index - 1)]
+    win_field = field_ + field_trans
+    win_field.append(field_diagonal_first)
+    win_field.append(field_diagonal_second)
+    win_comb = ['X'] * index
+    win_comb_ = ['0'] * index
+    for comb in win_field:
+        if comb == win_comb:
+            print('Выиграл игрок: 1')
+            win = 1
+            return win
+        elif comb == win_comb_:
+            print('Выиграл игрок: 2')
+            win = 2
+            return win
+    win = 0
+    return win
 
 
-def game_continue(win: int, move: int) -> int:
+def game_continue(win: int) -> int:
     """
     Функция проверки окончания игры после каждого хода
-    :param win: Результат функции проверки победы. 1-первый игрок выиграл, 2-второй, 0-никто
-    :param move: Введенный игроками номер клетки поля от 1 до 9
-    :return: 0 - игра продолжается, 1 - игра окончена
+    :param win: 1(первый игрок выиграл), 2(второй) или 0(никто)
+    :return continue_: 0 - игра продолжается, 1 - игра окончена
     """
     if win == 0:
         continue_ = 0
@@ -81,12 +83,13 @@ def game_continue(win: int, move: int) -> int:
         return continue_
 
 
-def player_step(field, move, current_player: str) -> list:
+def player_step(field: List[str], move: int, current_player: str) -> List[str]:
     """
-    Функция хода первого игрока. Переводит номер клетки, введенной игроком в "крестик"
+    Функция хода игроков
     :param field: Поле до хода игрока
     :param move: Введенный номер клетки игроком
-    :return: Новое поле после хода игрока
+    :param current_player: Крестик или нолик
+    :return field: Новое поле после хода игрока
     """
     move_ = move - 1
     for value in field:
@@ -94,33 +97,57 @@ def player_step(field, move, current_player: str) -> list:
             field[move_] = current_player
             return field
 
+
 def start_game():
+    """
+    Функция определения размерности поля в пределах от 3 до 5
+    :return index: Размерность поля
+    """
     print('Договоритесь, кто играет "Х", а кто "0"')
     while True:
-        index = int(input('Введите размерность поля от 2 до 5:'))
-        if index < 2 or index > 5:
-            print('Вы ввели неверное значение поля')
+        try:
+            index = int(input('Введите размерность поля от 2 до 5:'))
+            if index < 2 or index > 5:
+                print('Вы ввели неверное значение поля')
+                continue
+        except ValueError:
+            print('Вам необходимо ввести число!')
             continue
         return index
 
+
 def who_first() -> str:
+    """
+    Функция случайного определения, кто начинает игру
+    :return: Крестик или нолик
+    """
     list_ = ['X', '0']
     current_player = random.choice(list_)
     return current_player
 
+
 def game() -> None:
+    """
+    Функция хода игры
+    :return: None
+    """
     index = start_game()
     field = [' '] * pow(index, 2)
     draw_field(field, index)
     current_player = who_first()
-    print(f'Первым ходит {current_player}')
+    count_step = 0
+    print(f'Первым ходит "{current_player}"')
     while True:
         move = check_move(current_player, index, field)
         field = player_step(field, move, current_player)
+        count_step += 1
         draw_field(field, index)
-        win = who_win(field)
-        continue_ = game_continue(win, move)
+        win = who_win(field, index)
+        continue_ = game_continue(win)
         if win != 0 or continue_ != 0:
+            break
+        elif count_step == pow(index, 2):
+            print('Ничья!')
             break
         current_player = "X" if current_player == "0" else "0"
 
